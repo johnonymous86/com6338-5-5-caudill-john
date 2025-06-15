39501be730cece0bf30ba619060739021f7cba91
John Caudill <jcaudill86@gmail.com>
2025-06-15 14:46:19 -0400

    initial push

---
+++ index.js
@@ -0,0 +1,231 @@
+//ANY BUTTON INSIDE A FORM AUTO BECOMES A SUBMIT--TYPE
+//IS REDUNDANT
+//access form data by targeting input
+//and prevents form submission &
+//page reload
+
+
+
+//try a simple if/else statement first and build up from that. save your work and document
+
+var form = document.querySelector('form');
+var ul = document.querySelector('todo-list');
+var data = document.querySelector('input').value;
+var nodata = document.querySelector(!'input')
+var listButton = document.getElementById('li')
+
+
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+
+    if (data) {
+   
+    var text = document.createTextNode(data);
+    var newItem = document.createElement('li');
+  
+   
+    newItem.appendChild(text);
+    document.getElementById('todo-list').appendChild(newItem);
+    listButton.style = 'text-decoration: line';
+    }
+   
+    else if (nodata)
+        {return}
+
+    
+} 
+
+
+
+/*everything below this line was a failed attempt
+
+  listButton.onsubmit = function(e) {
+        e.preventDefault();
+        listButton.style = 'text-decoration: line';
+    }
+
+form.onsubmit = function(e) 
+    {e.preventDefault()}
+
+    if (data = !'input') {
+        console.log('null');
+    
+    } 
+    
+    else if (form.onsubmit = function(e) {
+        e.preventDefault();
+
+        var data = document.querySelector('input').value
+        var text = document.createTextNode(data)
+        var newItem = document.createElement('li')
+        newItem.appendChild(text)
+        document.getElementById('todo-list').appendChild(newItem);
+    }
+)
+
+
+
+
+
+form.onsubmit = function(e) {
+    
+    e.preventDefault();
+
+if (data = 'input'){
+
+    var data = document.querySelector('input').value
+    var text = document.createTextNode(data) 
+    var newItem = document.createElement('li')
+    newItem.appendChild(text)
+    document.getElementById('todo-list').appendChild(newItem)
+}
+else if (data = '' || '  '){
+    var data = document.querySelector('input').value
+    console.log('nothing')
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var form = document.querySelector('form')
+var ul = document.querySelector('todo-list')
+var data = document.querySelector('input').value
+
+
+
+form.onsubmit = function(e) {
+    
+    e.preventDefault();
+
+    var text = document.createTextNode(data) 
+    var newItem = document.createElement('li')
+    newItem.appendChild(text)
+    document.getElementById('todo-list').appendChild(newItem)
+
+
+}
+
+
+
+
+document.addEventListener('keydown', function(e) 
+    if (e.key === data ){
+
+
+form.onsubmit =  function(e) {
+    e.preventDefault();
+    console.log('h')
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* OLD CODE
+document.addEventListener('keydown', function(event) {
+    const displayDiv = document.getElementById('todo-list');
+    displayDiv.textContent += event.key;
+  });
+
+
+function logSubmit(e) {
+    console.log = '!';
+    e.preventDefault();
+    
+}
+
+const button = document.getElementsByTagName('button');
+
+addEventListener(button, (event) => { })
+
+onsubmit = (event) => { }
+
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("todo-list").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+
+/*selects button without tag
+var allButtons = document.getElementsByTagName("button");
+var buttonsWithoutAttributes = [];
+
+for (var i = 0; i < allButtons.length; i++) {
+  if (allButtons[i].attributes.length === 0) {
+    buttonsWithoutAttributes.push(allButtons[i]);
+  }
+}
+*/
+
+
+
